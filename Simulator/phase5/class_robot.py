d4237574099000b685055b8df73c0abd084504ce
Mariaapalacios <mp352@njit.edu>
2025-06-06 21:04:02 -0400

    o sea igual aun falta, pero ya entendi kalogica

---
+++ Simulator/phase5/class_robot.py
@@ -0,0 +1,19 @@
+import pygame
+import math
+import random
+from class_logic import Logic
+class Robot:
+    
+    def __init__(self, robot_id, x, y, radius):
+        self.robot_id = robot_id
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.hop_count = None
+        self.parent = None
+
+    def draw(self, screen, color=(0, 200, 0)):
+        pygame.draw.circle(screen, color, (int(self.x), int(self.y)), self.radius)
+        font = pygame.font.Font(None, 20)
+        label = font.render(str(self.robot_id), True, (0, 0, 0))
+        screen.blit(label, (self.x - 10, self.y - 25))
