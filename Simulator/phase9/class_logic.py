2317a7a83dab8014da06c9b6e7eb209bda31a172
Mariaapalacios <mp352@njit.edu>
2025-09-22 10:42:48 -0400

    added to phase9

---
+++ Simulator/phase9/class_logic.py
@@ -0,0 +1,27 @@
+import math
+
+class Logic:
+    def __init__(self, angle):
+        self.angle = angle
+        self.drive_speed = 3
+        self.color = [0, 150, 0]  
+
+    def get_drive_speed(self):
+        return self.drive_speed
+
+    def get_angle(self):
+        return self.angle
+
+    def get_color(self):
+        return self.color
+
+    def update(self, dx, dy):
+        # angle towards the target
+        self.angle = math.atan2(dy, dx)
+        
+
+    def reset_speed(self):
+        self.drive_speed = 3
+
+    def stop(self):
+        self.drive_speed = 0
